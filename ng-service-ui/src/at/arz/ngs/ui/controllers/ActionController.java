802a15d2e0eb37cc3778608ac1c1e287f1863b91
Dani <daniel.testor@hotmail.com>
2016-08-15 13:58:50 +0200

    tried to add an commandButton for start, but doesnt invoke start method

--- ng-service-ui/src/at/arz/ngs/ui/controllers/ActionController.java
+++ ng-service-ui/src/at/arz/ngs/ui/controllers/ActionController.java
@@ -1,5 +1,6 @@
 package at.arz.ngs.ui.controllers;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -8,28 +9,32 @@ import at.arz.ngs.serviceinstance.ServiceInstanceAdmin;
 import at.arz.ngs.serviceinstance.commands.action.PerformAction;
 
 @RequestScoped
-@Named("action")
+@Named("actionController")
 public class ActionController {
 	
 	@Inject
 	private ServiceInstanceAdmin service;
 	
+	@PostConstruct
+	public void init() {
+		System.out.println("\n\n\n\n\\n\n\nTEST");
+	}
 	
 	public void start() {
 		PerformAction action = new PerformAction();
 		action.setPerformAction("start");
-		service.performAction("arctis", "pebk123", "ln003", "arctis_1", action);
+		service.performAction("arctis", "pebk123", "lnx003", "arctis_1", action);
 	}
 	
 	public void stop() {
 		PerformAction action = new PerformAction();
 		action.setPerformAction("stop");
-		service.performAction("arctis", "pebk123", "ln003", "arctis_1", action);
+		service.performAction("arctis", "pebk123", "lnx003", "arctis_1", action);
 	}
 	
 	public void restart() {
 		PerformAction action = new PerformAction();
 		action.setPerformAction("restart");
-		service.performAction("arctis", "pebk123", "ln003", "arctis_1", action);
+		service.performAction("arctis", "pebk123", "lnx003", "arctis_1", action);
 	}
 }
