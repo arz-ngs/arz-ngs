318a79de80ea79cd11a7d4d9e79824f2fd463493
Alexander Schiegl <alexander.schiegl@gmx.at>
2016-09-01 13:03:40 +0200

    added restart for ScriptExecutor and added support for exitCodeDummy in curl_input.sh

--- ng-service-ejb/lib/scripts/performancetest_curl_input.sh
+++ ng-service-ejb/lib/scripts/performancetest_curl_input.sh
@@ -13,24 +13,43 @@ do
        		count_instance=0
     		while [ "$count_instance" -lt $count_per_element ]  
     		do
-			   service="service_$count_service"
-			   env="env_$count_env"
-			   host="host_$count_host"
-			   instance="instance_$count_instance"
+			    service="service_$count_service"
+			    env="env_$count_env"
+			    host="host_$count_host"
+			    instance="instance_$count_instance"
 			   
-			   x='{"environmentName":"'
-			   y='", "hostName":"'
-			   a='", "serviceName":"'
-	   		   b='", "instanceName":"'
-	   		   c='", "script": {"pathStart":"init.d/arctis start", "pathStop":"init.d/arctis stop", "pathRestart":"init.d/arctis restart", "pathStatus":"init.d/arctis status"}}'
-	           
-	           json_string="$x$env$y$host$a$service$b$instance$c"
-	   	 	   
-	   	 	   echo "adding instance: "
-	   	 	   echo "$json_string"
-	   		   curl -H "Content-Type: application/json" -X PUT -d "$json_string" http://localhost:8080/ngs/api/v1/instances
+			    x='{"environmentName":"'
+			    y='", "hostName":"'
+			    a='", "serviceName":"'
+	   		    b='", "instanceName":"'
+	   		    c='", "script": {"pathStart":"'
+				e='", "pathStop":"'
+				f='", "pathRestart":"'
+				g='", "pathStatus":"'
+				h='"}}"'
 			   
-			   count_instance=`expr $count_instance + 1`
+			    SCRIPT=$(readlink -f "$0")
+				# Absolute path this script is in, thus /home/user/bin
+				SCRIPTPATH=$(dirname "$SCRIPT")
+				
+				string_to_replace_d=D:
+				SCRIPTPATH="${SCRIPTPATH/\/d/$string_to_replace_d}"
+				string_to_replace_c=C:
+				SCRIPTPATH="${SCRIPTPATH/\/c/$string_to_replace_c}"
+				SCRIPTPATH="$SCRIPTPATH/exitCodeDummy.bat"
+				
+				predict="predict 0"
+				start_path="$SCRIPTPATH start $predict"
+				stop_path="$SCRIPTPATH stop $predict"
+				restart_path="$SCRIPTPATH restart $predict"
+				status_path="$SCRIPTPATH status $predict"
+				
+				json_string="$x$env$y$host$a$service$b$instance$c$start_path$e$stop_path$f$restart_path$g$status_path$h" 
+				echo "adding instance: "
+				echo "$json_string"
+	   		    curl -H "Content-Type: application/json" -X PUT -d "$json_string" http://localhost:8080/ngs/api/v1/instances
+				
+			    count_instance=`expr $count_instance + 1`
 		    done 
 		    echo
 		    count_host=`expr $count_host + 1`
